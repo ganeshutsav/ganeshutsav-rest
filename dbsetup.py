38a1186e628afcfd6d116b1d7d6c5cd77c85caa3
Suresh Sarda <sureshssarda@gmail.com>
2017-05-31 20:06:19 +0530

    Add config file and dbsetup file

---
+++ dbsetup.py
@@ -0,0 +1,18 @@
+import logging
+import pymongo
+import sys
+import yaml
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+log = logging.getLogger(__name__)
+
+if __name__ == '__main__':
+    config = yaml.load(open('config.yaml', 'r')).get('database')
+
+    db = pymongo.MongoClient().get_database(config['db_name'])
+    collections = config['collections']
+
+    for collection in collections:
+        log.info('Recreating collection: {0}'.format(collection))
+        db.drop_collection(collection)
+        db.create_collection(collection)
