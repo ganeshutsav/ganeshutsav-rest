aa77cfab1583050dd568576063a6048e324b716d
Suresh Sarda <sureshssarda@gmail.com>
2017-06-01 19:49:55 +0530

    Base requests

---
+++ rest/common/model/request.py
@@ -0,0 +1,22 @@
+class BaseRequest(object):
+    pass
+
+
+class CreateRequest(BaseRequest):
+    pass
+
+
+class UpdateRequest(BaseRequest):
+    pass
+
+
+class DeleteRequest(BaseRequest):
+    pass
+
+
+class RetrieveRequest(BaseRequest):
+    pass
+
+
+class ListRequest(BaseRequest):
+    pass
